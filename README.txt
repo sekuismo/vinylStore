22b80c739be22d9b7a21b9c55a80e87c29a7ca51
Esteban Muñoz <estebanmunozrojas@gmail.com>
2022-12-21 11:51:44 -0300

    avance renderizado

---
+++ README.txt
@@ -0,0 +1,33 @@
+La función addToShoppingList se utiliza para agregar un disco a la lista de compras. 
+Esta función se llama cuando se hace clic en el botón "Agregar" junto a cada disco en 
+la lista de discos disponibles. La función toma un disco como argumento y agrega este 
+disco a la lista de compras existente utilizando la spread operator (...). Luego, la
+ función almacena la lista actualizada de compras en el almacenamiento local del navegador
+  utilizando el método setItem de localStorage.
+
+La función getShoppingList se utiliza para obtener la lista de compras almacenada en el 
+almacenamiento local del navegador. Esta función se llama cuando se hace clic en el botón
+ "Ver lista de compras". La función obtiene el valor almacenado en el almacenamiento local
+  utilizando el método getItem de localStorage, lo parsea a un objeto JavaScript y lo devuelve.
+
+La función removeFromShoppingList se utiliza para eliminar un disco de la lista de compras. 
+Esta función se llama cuando se hace clic en el botón "Eliminar" junto a cada disco en la 
+lista de compras. La función toma un disco como argumento y filtra la lista de compras para 
+eliminar el disco seleccionado. Luego, la función almacena la lista actualizada de compras en 
+el almacenamiento local del navegador utilizando el método setItem de localStorage.
+
+La función handleClick se utiliza para mostrar la lista de compras almacenada en el almacenamiento 
+local del navegador. Esta función se llama cuando se hace clic en el botón "Ver lista de compras".
+ La función llama a getShoppingList para obtener la lista de compras almacenada y luego actualiza
+  el estado de la aplicación con la lista obtenida.
+
+La función handleSubmit se utiliza para manejar el evento de envío de un formulario. Esta función
+ se llama cuando se hace clic en el botón "Enviar" del formulario de "Solicitar disco". La función
+  hace lo siguiente:
+
+Previene el comportamiento predeterminado del evento de envío de formulario, que suele ser enviar
+ los datos del formulario a una URL especificada.
+Crea una nueva instancia de FormData a partir del formulario que ha sido enviado.
+Obtiene los valores de los campos "nombreDisco", "interprete" y "estilo" del formulario utilizando 
+el método get de FormData.
+Crea un nuevo objeto
--- src/componentes/Filtrar.jsx
+++ src/componentes/Filtrar.jsx
@@ -14,41 +14,89 @@ const Filtrar = () => {
     }
     fetchData();
   }, []);
+  
 
   function addToShoppingList(disco) {
-    setShoppingList([...shoppingList, disco]);
-    localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+    const updatedShoppingList = [...shoppingList, disco];
+    setShoppingList(updatedShoppingList);
+    localStorage.setItem('shoppingList', JSON.stringify(updatedShoppingList));
   }
+  
+  function removeFromShoppingList(disco) {
+    const updatedShoppingList = shoppingList.filter(item => item !== disco);
+    setShoppingList(updatedShoppingList);
+    localStorage.setItem('shoppingList', JSON.stringify(updatedShoppingList));
+  }
+  
   
   function getShoppingList() {
     const shoppingListString = localStorage.getItem('shoppingList');
     return JSON.parse(shoppingListString);
   }
-  function removeFromShoppingList(disco) {
-    setShoppingList(shoppingList.filter(item => item !== disco));
-    localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-  }
   function handleClick() {
     const shoppingList = getShoppingList();
     setShoppingList(shoppingList);
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+  
+    const formData = new FormData(event.target);
+    const nombreDisco = formData.get('nombreDisco');
+    const interprete = formData.get('interprete');
+    let precio= formData.get('precio');
+    precio = parseFloat(precio)
+    let year = formData.get('year');
+    year =parseInt(year)
+    const estilo = formData.get('estilo');
+    let stock = formData.get('stock')
+    stock = parseInt(stock)
+
+    const nuevoDisco = {
+      id: Date.now(), // Asignar un valor único como id
+      nombreDisco,
+      interprete,
+      estilo,
+      precio,
+      year,
+      stock
+    };
+    
+    fetch('http://localhost:3004/discos', {
+      method: 'POST',
+      body: JSON.stringify(nuevoDisco),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        setDiscos([...discos, data]);
+        // Agrega el nuevo disco al carrito de compras
+        setShoppingList([...shoppingList, data]);
+        // Almacena la lista de compras en localStorage
+        localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+      });
+  }
+
+  
+  
+  
   
 
   return (
+    
     <Fragment>
       <Container>
-        <Row>
-          <Col>
-            <h1 className="display-2">Discos</h1>
+        <Row style={{margin:'20px'}}>
+          <Col >
+            <h1 className="display-2 ">Agrega discos</h1>
           </Col>
         </Row>
         <Row>
-          <Col>
-          
-          </Col>
-          <Col>
+
+          <Col sm={4}>
             {discos.map(disco => (
-              <div style={{ border: '1px solid black', padding: '10px' }} key={disco.nombreDisco}>
+              <div style={{ border: '1px solid black', padding: '10px' }} key={disco.id}>
                 <p>
                   {disco.nombreDisco} - {disco.interprete} - {disco.estilo}
                 </p>
@@ -63,7 +111,7 @@ const Filtrar = () => {
               </div>
             ))}
           </Col>
-          <Col>
+          <Col sm={4}>
             <h2>Lista de compra</h2>
             <br/>
             <Button onClick={handleClick}>Ver lista de compras</Button>
@@ -85,6 +133,49 @@ const Filtrar = () => {
     
 </div>
 ))}
+</Col>
+<Col sm={4}>
+    <h2 className='display-3'>Solicitar disco</h2>
+    <br />
+    <p>Encarga un disco que no tengamos en nuestra lista</p>
+    <br />
+
+    <form onSubmit={handleSubmit}>
+  <label>
+    Nombre del disco:
+    <input type="text" name="nombreDisco" />
+  </label>
+  <br />
+  <label>
+    Interprete:
+    <input type="text" name="interprete" />
+  </label>
+  <br />
+  <label>
+    Precio:
+    <input type="number" name="precio" />
+  </label>
+  <br />
+  <label>
+    Año:
+    <input type="number" name="year" />
+  </label>
+  <br />
+  <label>
+    Estilo:
+    <input type="text" name="estilo" />
+  </label>
+  <br />
+  <label>
+    Stock:
+    <input type="number" name="stock" />
+  </label>
+  <br />
+  <button type="submit">Enviar</button>
+</form>
+<br />
+
+
 </Col>
 </Row>
 </Container>
